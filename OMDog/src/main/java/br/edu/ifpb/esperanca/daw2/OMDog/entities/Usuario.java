eef5876da7e01614a5775b48d851b19c997cfdc6
brunalu28 <bluiza747@gmail.com>
2019-11-08 13:20:49 -0200

    Atualizado 08/11

--- OMDog/src/main/java/br/edu/ifpb/esperanca/daw2/OMDog/entities/Usuario.java
+++ OMDog/src/main/java/br/edu/ifpb/esperanca/daw2/OMDog/entities/Usuario.java
@@ -29,6 +29,8 @@ public class Usuario implements Identificavel {
 	private String password;
 	private String username;
 	private String grupo;
+	private String comentariodousuario;
+	private String avaliacaodousuario;
 	
 	@OneToMany(mappedBy="usuario")
 	private List<Postagem> postagens;
@@ -92,15 +94,37 @@ public class Usuario implements Identificavel {
 		this.telefone = telefone;
 	}
 
+	public String getComentariodousuario() {
+		return comentariodousuario;
+	}
+
+	public void setComentariodousuario(String comentariodousuario) {
+		this.comentariodousuario = comentariodousuario;
+	}
+
+	public String getAvaliacaodousuario() {
+		return avaliacaodousuario;
+	}
+
+	public void setAvaliacaodousuario(String avaliacaodousuario) {
+		this.avaliacaodousuario = avaliacaodousuario;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((avaliacaodousuario == null) ? 0 : avaliacaodousuario.hashCode());
+		result = prime * result + ((comentariodousuario == null) ? 0 : comentariodousuario.hashCode());
 		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((grupo == null) ? 0 : grupo.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((nascimento == null) ? 0 : nascimento.hashCode());
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((postagens == null) ? 0 : postagens.hashCode());
 		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
 		return result;
 	}
 
@@ -113,10 +137,25 @@ public class Usuario implements Identificavel {
 		if (getClass() != obj.getClass())
 			return false;
 		Usuario other = (Usuario) obj;
+		if (avaliacaodousuario == null) {
+			if (other.avaliacaodousuario != null)
+				return false;
+		} else if (!avaliacaodousuario.equals(other.avaliacaodousuario))
+			return false;
+		if (comentariodousuario == null) {
+			if (other.comentariodousuario != null)
+				return false;
+		} else if (!comentariodousuario.equals(other.comentariodousuario))
+			return false;
 		if (email == null) {
 			if (other.email != null)
 				return false;
 		} else if (!email.equals(other.email))
+			return false;
+		if (grupo == null) {
+			if (other.grupo != null)
+				return false;
+		} else if (!grupo.equals(other.grupo))
 			return false;
 		if (id == null) {
 			if (other.id != null)
@@ -133,14 +172,32 @@ public class Usuario implements Identificavel {
 				return false;
 		} else if (!nome.equals(other.nome))
 			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (postagens == null) {
+			if (other.postagens != null)
+				return false;
+		} else if (!postagens.equals(other.postagens))
+			return false;
 		if (telefone == null) {
 			if (other.telefone != null)
 				return false;
 		} else if (!telefone.equals(other.telefone))
 			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
 		return true;
 	}
+	
+	
 
+	
 	
 	
 	
